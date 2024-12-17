a8a6144cf4dfdc2635bb4fa7b6a675e1411c76dd
P-ict0 <rodrigomartinnunez@gmail.com>
2024-12-17 12:53:36 +0100

    (filestructure) Initial structure

---
+++ src/utils/constants.py
@@ -0,0 +1,11 @@
+# Default categories for accounts
+DEFAULT_ACCOUNT_CATEGORIES = ["Savings", "Investment", "Cash"]
+
+# Supported file formats
+SUPPORTED_FILE_TYPES = ["pdf", "csv", "xlsx"]
+
+# Default currency
+DEFAULT_CURRENCY = "EUR"
+
+# Error messages
+ERROR_INVALID_FILE = "The uploaded file format is not supported."
